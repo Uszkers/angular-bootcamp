30b4a845c167d53cc6dd84051c0ca4e2bea3901d
Mateusz Kawa <matekawa@windowslive.com>
2023-05-30 13:30:36 +0200

    Chapter 1.2: * odświeżenie brancha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'abc-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  todos = [
+    {
+      title: 'Skończyć pisanie sprawozdania',
+      description:
+        'Dokończyć pisanie sprawozdania z wdrożenia przed ustalonym terminem',
+      dueDate: '2023-06-10',
+      priority: 'HIGH',
+      completed: false,
+    },
+    {
+      title: 'Zrobić zakupy',
+      description: 'Kupić niezbędne produkty na cały tydzień',
+      dueDate: '2023-06-02',
+      priority: 'MEDIUM',
+      completed: false,
+    },
+    {
+      title: 'Ćwiczenia',
+      description: 'Wstać wcześniej i pójść na siłownię',
+      dueDate: '2023-06-05',
+      priority: 'LOW',
+      completed: false,
+    },
+  ];
 }
