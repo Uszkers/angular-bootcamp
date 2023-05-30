46bf9773f0b139ea1c13972ee925100f19b166c2
Mateusz Kawa <matekawa@windowslive.com>
2023-05-30 22:03:49 +0200

    Chapter 1.4: * splitting into components and i/o

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,29 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  todos = [
-    {
-      title: 'Skończyć pisanie sprawozdania',
-      description:
-        'Dokończyć pisanie sprawozdania z wdrożenia przed ustalonym terminem',
-      dueDate: '2023-06-10',
-      priority: 'HIGH',
-      completed: false,
-    },
-    {
-      title: 'Zrobić zakupy',
-      description: 'Kupić niezbędne produkty na cały tydzień',
-      dueDate: '2023-06-02',
-      priority: 'MEDIUM',
-      completed: false,
-    },
-    {
-      title: 'Ćwiczenia',
-      description: 'Wstać wcześniej i pójść na siłownię',
-      dueDate: '2023-06-05',
-      priority: 'LOW',
-      completed: false,
-    },
-  ];
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todos/todo-list/todo-list.component';
+import { TodoComponent } from './todos/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
