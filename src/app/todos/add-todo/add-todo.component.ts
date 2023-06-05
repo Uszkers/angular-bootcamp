12ae766afdc89e1a87ef9e9f93abca95667c684f
Mateusz Kawa <matekawa@windowslive.com>
2023-06-05 23:33:26 +0200

    Chapter 2.2: * filling add-todo.component.html * changes in colors * adding roboto font

--- src/app/todos/add-todo/add-todo.component.ts
+++ src/app/todos/add-todo/add-todo.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { PRIORITY_DICTIONARY, TodoModel } from '../todo/todo.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'abc-add-todo',
   templateUrl: './add-todo.component.html',
   styleUrls: ['./add-todo.component.scss'],
 })
-export class AddTodoComponent {}
+export class AddTodoComponent {
+  model: TodoModel = {
+    title: '',
+    completed: false,
+    dueDate: undefined,
+    priority: 'LOW',
+    description: '',
+  };
+
+  priorityDict = PRIORITY_DICTIONARY;
+  minDate = new Date().toISOString().split('T')[0];
+
+  onSubmit(addTodoForm: NgForm): void {
+    if (addTodoForm.invalid) {
+      addTodoForm.form.markAllAsTouched();
+      console.error('Form is invalid');
+      return;
+    }
+    console.log('TODO: handle adding data on submit', this.model);
+  }
+}
